81406d5b8ee5056d1fd242e56bc6993ff3eb436d
rticgms <38751580+rticgms@users.noreply.github.com>
2018-07-05 12:37:14 +0200

    Remove fingerprint

--- public_html/create.js
+++ public_html/create.js
@@ -4,10 +4,9 @@ eventHandler.addEventListener('submit', function(event) { generate(event); })
 function generate(event) {
     event.preventDefault();
 
-    var fp = new Fingerprint().get();
     var pw = document.getElementById("password").value.trim();
     var un = document.getElementById("username").value.trim();
-    var key = CryptoJS.AES.encrypt(pw, un +''+ fp).toString().trim();
+    var key = CryptoJS.AES.encrypt(pw, un).toString().trim();
     document.location.href = "index.html?username=" + un + "&key=" + encodeURIComponent(key);
     return false
 }
